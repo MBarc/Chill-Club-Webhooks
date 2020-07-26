c57c660880e059e15f564b1633180afc649f6868
MBarc <42979055+MBarc@users.noreply.github.com>
2020-07-26 12:01:20 -0400

    Update serverstatuswebhook.py

--- serverstatuswebhook.py
+++ serverstatuswebhook.py
@@ -3,49 +3,49 @@ from discord_webhook import DiscordWebhook
 
 services = {
     'Call of Duty': {
-        'webhook': 'https://discordapp.com/api/webhooks/727289363175702680/_wcOyVKx3lO8lpEKYK-E_B3KZiV02opZL8H6Ylk1pzYlX91WPDlERjWvpH4TdgQFSV36',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/call-of-duty/',
         'roleID': '727221234429067295',
         'status': 'online'
     },
     'Destiny': {
-        'webhook': 'https://discordapp.com/api/webhooks/727744001347813406/XtD6rbzrxosf22rWK6CmZK3k1y2_IGZ0pI9QgBA-sk5_XD2UTgjEafZBfFqOLOUzxai6',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/destiny/',
         'roleID': '727221199037399061',
         'status': 'online'
     },
     'Fortnite': {
-        'webhook': 'https://discordapp.com/api/webhooks/727737910744449085/oGbwBndH-Nf6fBXYgJjbAM5UNBGriZk7ReZ6Es7x-blmhSsJvl8NU-JzbxmHg95Av8iM',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/fortnite/',
         'roleID': '727505873613881426',
         'status': 'online'
     },
     'Nintendo Switch Online': { # used by the following categories [Animal Crossing]
-        'webhook': 'https://discordapp.com/api/webhooks/727737354147856418/ktJOo-qbScfxFlxNda4cMM9hgFTCu6igkvnXpq2S30dC_PINFgHkWmsZCbcnPk1sCwG8',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/nintendo-switch-online/',
         'roleID': '727221111531503686', #animal crossing role ID
         'status': 'online'
     },
     'Overwatch': {
-        'webhook': 'https://discordapp.com/api/webhooks/727740100183851069/tHfDu958gaXonN2sRVvhrqTjE3QOW26P1H35SqJjUxFJdHjB6jr_A9FMewP-gXeC-z8y',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/overwatch/',
         'roleID': '727505803711479879',
         'status': 'online'
         },
     'Rainbow 6': {
-        'webhook': 'https://discordapp.com/api/webhooks/727741184734396417/FM1p4VUMhG_OcQjA0YbV04wsfzgIVPClKq4KtC-fkwYtejncS0jy_2Fc3UceZaHLudv4',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/rainbow-six/',
         'roleID': '727505837953646632',
         'status': 'online'
     },
     'Rocket League': {
-        'webhook': 'https://discordapp.com/api/webhooks/727811332367450123/2sSNwF1YFzRGWplRon8-MUpgf-HH8iuklSJALWgZC9wAQj0dZXQZiXFkoPhTiu6E3zvA',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/rocket-league/',
         'roleID': '727221384539013121',
         'status': 'online'
     },
     'The Sims': {
-        'webhook': 'https://discordapp.com/api/webhooks/727743037652074517/TdXx6HML1-b3E3_eEhhOJOq0ci-kfZQeO_7cHrP4znwoatjg8DCf3hX5dPPB8NpUyprd',
+        'webhook': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
         'url': 'https://downdetector.com/status/the-sims-4/',
         'roleID': '727221741071368203',
         'status': 'online'
