184fb9f88efa3b2c86eef6247e003d3d86efc1d9
MBarc <42979055+MBarc@users.noreply.github.com>
2020-07-26 11:58:43 -0400

    Update nasaapodwebhook.py

--- nasaapodwebhook.py
+++ nasaapodwebhook.py
@@ -2,14 +2,14 @@ import requests, json
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import pprint
 #Getting data from Nasa's A.P.O.D.
-api_key = 'fezcl1LeViOKgNbcpWHSetE75fPvz7BdiFHaaVXd'
+api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 r = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}').text
 data = json.loads(r)
 
 pprint.pprint(data)
 
 #Initializing webhook message and embedding content into it
-webhookURL = 'https://discordapp.com/api/webhooks/728690954764288092/RO8j80MF1rJuXCsw-JHrtQBuaQ4pODxEjCjLZc1o4NGJfNjiBLI3KsB-Vi0BZLKT4-B9'
+webhookURL = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 webhook = DiscordWebhook(url=webhookURL)
 embed = DiscordEmbed(title=data['title'], description=data['explanation'], color=1000)
 embed.set_image(url=data['url'])
