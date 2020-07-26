4bdbd30244feb7abf6f65460a52692e709a9c2d8
MBarc <42979055+MBarc@users.noreply.github.com>
2020-07-26 11:58:15 -0400

    Update marsroverwebhook.py

--- marsroverwebhook.py
+++ marsroverwebhook.py
@@ -2,11 +2,11 @@ import requests, datetime
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 yesterday = f'{datetime.datetime.now().year}-{datetime.datetime.now().month}-{datetime.datetime.now().day - 3}' # -1 to get yesterday's date
-api_key = 'fezcl1LeViOKgNbcpWHSetE75fPvz7BdiFHaaVXd'
+api_key = 'XXXXXXXXXXXXXXXXXXXXX'
 r = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={yesterday}&camera=fhaz&api_key={api_key}')
 
 try:
-    webhookURL = 'https://discordapp.com/api/webhooks/729050571700568065/NaR8XFpShKy1EZPPlCKQMMT9xSqjq2cftlK67YEsnC1sYSMmAvHELUWKRbSAnN5FlwGG'
+    webhookURL = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     webhook = DiscordWebhook(url=webhookURL)
 
     embed = DiscordEmbed(title=f"Taken on {r.json()['photos'][0]['earth_date']}", color=15170833)#color == hex value converted to decimal
