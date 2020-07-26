a09a8333ec47fae0b5090edf8b36eb20775e8604
MBarc <42979055+MBarc@users.noreply.github.com>
2020-07-26 11:55:37 -0400

    Update animalcrossingnewswebhook.py

--- animalcrossingnewswebhook.py
+++ animalcrossingnewswebhook.py
@@ -2,10 +2,10 @@ import tweepy
 from discord_webhook import DiscordWebhook
 import datetime as dt
 
-consumerKey = "xzKmPYUndQyF3sW8Yzs7A6Ia4"
-consumerSecret = "bC9DMdqbonyvphhoX0vbNKfKXtyXhyIAZQagugtqUW5R6p0bcz"
-accessToken = "925489542-bK3Kbf62l9ludW3fahrY3sIjHoPKCeA5h0UJR9my"
-accessTokenSecret = "sPDY4prKtPwgg4c63VzXr1d2cRGQFyH2aGxLQLwwM5tBe"
+consumerKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+consumerSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+accessToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+accessTokenSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
@@ -28,7 +28,7 @@ endTime = dt.datetime.utcnow()
 
 mostRecentTweet = newsTweets[0]
 if mostRecentTweet.created_at < endTime and mostRecentTweet.created_at > startTime:
-    webhook = 'https://discordapp.com/api/webhooks/729417210124304424/J2N5sQsu5vZfZDF4pnBu-YM5jn8UnTltT-aRVIQPdIYZ0Kog9JAz-aPBMvzk4M5NmS0E'
+    webhook = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     webhookContent = f'https://twitter.com/ACWorldBlog/status/{mostRecentTweet.id}'  # Exception would be here
     webhook = DiscordWebhook(url=webhook, content=webhookContent)
     response = webhook.execute()
