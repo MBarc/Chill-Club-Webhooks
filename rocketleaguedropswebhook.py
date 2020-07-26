d29ebf6e1c73ac7eaae4466b975ea34b6e0c125e
MBarc <42979055+MBarc@users.noreply.github.com>
2020-07-26 12:00:10 -0400

    Update rocketleaguedropswebhook.py
    Fixed typo within a string.

--- rocketleaguedropswebhook.py
+++ rocketleaguedropswebhook.py
@@ -12,7 +12,7 @@ amountOfViews = jsonData['chatter_count']
 if amountOfViews >= 5000: # 5000 people to know that the Rocket League channel is stream (as opposed to hosting another channel).
 
     webhook = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-    webhookContent = f'Rock League is live on Twitch Right now! View the stream here <https://www.twitch.tv/rocketleague>'
+    webhookContent = f'Rocket League is live on Twitch Right now! View the stream here <https://www.twitch.tv/rocketleague>'
     webhook = DiscordWebhook(url=webhook, content=webhookContent)
 
     response = webhook.execute()
